7c31b2c568fb93dd552f9e05ff326e68fa2463f7
Nicolas Peckman <npeckman@extron.com>
2017-11-20 16:21:48 -0500

    database ops

--- src/database.nim
+++ src/database.nim
@@ -1,4 +1,27 @@
 import times
+import db_sqlite
+
+type
+  Database* = ref object
+    db: DbConn
+
+proc newDatabase*(filename = "tweeter.db"): Database =
+  new result
+  result.db = open(filename, "", "", "")
+
+proc post*(database: Database, message: Message) =
+  if message.msg.len > 140:
+    raise newException(ValueError, "Message must be less than 140 characters"
+
+  database.db.exec(sql"INSERT INTO Message VALUES (?, ?, ?);",
+    message.username, $message.time.toSeconds().int, message.msg)
+
+proc follow*(database: Database, follower: User, user: User) = 
+  database.db.exec(sql"INSERT INTO Following VALUES (?, ?);",
+    follower.username, user.username)
+
+proc create*(database: Database, user: User) =
+  database.db.exec(sql"INSERT INTO User VALUES (?);", user.username)
 
 type 
   User* = object
